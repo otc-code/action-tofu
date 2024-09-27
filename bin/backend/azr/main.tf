cea54c51036e7c7b4c9729f85e1b2ce09a38d95d
Joerg Kritzen <j.kritzen@ontracon.de>
2024-09-27 12:53:51 +0200

    fix: checkov for azure

--- bin/backend/azr/main.tf
+++ bin/backend/azr/main.tf
@@ -12,13 +12,21 @@ resource "azurerm_storage_account" "tfstate" {
   location                 = azurerm_resource_group.tfstate.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
   tags = {
     Managed_by = "CI/CD: ${var.workflow}"
   }
 }
 
 resource "azurerm_storage_container" "tfstate" {
-  name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "private"
+  name                            = var.container_name
+  storage_account_name            = azurerm_storage_account.tfstate.name
+  container_access_type           = "private"
+  allow_nested_items_to_be_public = false
 }
