407b078233bf58d549e7f58d976f08f5091b5429
Joerg Kritzen <j.kritzen@ontracon.de>
2024-11-12 20:59:41 +0100

    fix: skip push on renovate branches`

--- bin/functions.sh
+++ bin/functions.sh
@@ -226,7 +226,7 @@ function gh_annotation() {
 }
 
 git_push() {
-	if [[ ! $GITHUB_EVENT_NAME == "pull_request" ]] && [[ ! $REF == "renovate/*" ]]; then
+  if [[ ! $GITHUB_EVENT_NAME == "pull_request" ]] && [[ ! $REF =~ ^renovate/.* ]]; then
 		MINWAIT=1
 		MAXWAIT=45
 		if [[ "$CI" == "true" ]]; then
